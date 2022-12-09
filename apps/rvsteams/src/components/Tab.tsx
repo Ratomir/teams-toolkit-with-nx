59216f80c14d9a3fbfdd268cd8de83dcf3ecd530
ratomir.vukadin <ratomir.vukadin@nulia.com>
2022-12-09 11:49:01 +0100

    new changes

---
+++ apps/rvsteams/src/components/Tab.tsx
@@ -0,0 +1,14 @@
+import { Banner } from '@myorg/common-ui';
+import React from "react";
+
+class Tab extends React.Component {
+  render() {
+    return (
+      <div>
+        <Banner text="Welcome to our admin app." />
+      </div>
+    );
+  }
+}
+
+export default Tab;
